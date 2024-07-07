7383fc890904a897263597b342c8c2093eab8d54
mzhang8132 <mzhang8132@gmail.com>
2024-07-07 00:16:36 -0600

    Completed: Day 1

---
+++ practice/day1.py
@@ -0,0 +1,51 @@
+print(3 + 4)    # add
+print(3 - 4)    # minus
+print(3 * 4)    # multiply
+print(3 % 4)    # modulus
+print(3 / 4)    # divide
+print(3**4)   # exponential
+print(3 // 4)   # floor divide
+
+print("Your name")
+print("Your family name")
+print("Your country")
+print("I am enjoying 30 days of python")
+
+print(type(10))                                 # int
+print(type(9.8))                                # float
+print(type(3.14))                               # float
+print(type(4 - 4j))                             # complex
+print(type(["Asabeneh", "Python", "Finland"]))  # list
+print(type("Your name"))                        # string
+print(type("Your family name"))                 # string
+print(type("Your country"))                     # string
+
+# int
+i = 1
+
+# float
+f = 1.1
+
+# complex
+c = 1 + 1j
+
+# string
+st = "one"
+
+# boolean
+b = True
+
+# list
+l = ["one", 1, "two" , 2]
+
+# tuple
+t = ("one", 1)
+
+# set
+s = {1, 2, 3}
+
+# dictionary
+d = {"one":1, "two":2}
+
+# euclidean distance
+dist = (2 - 10)**2 + (3 - 8)**2
