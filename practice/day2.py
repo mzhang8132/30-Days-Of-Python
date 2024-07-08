9959c909a49c2b9f2a223a6089eb96ab86b6fc73
mzhang8132 <mzhang8132@gmail.com>
2024-07-08 00:33:10 -0600

    Completed: Day 2

---
+++ practice/day2.py
@@ -0,0 +1,49 @@
+# Day 2: 30 Days of python programming
+
+first_name = "Min"
+last_name = "Zhang"
+full_name = "Min Zhang"
+country = "USA"
+city = "El Paso"
+age = 23
+year = 2024
+is_married = False
+is_true = True
+is_light_on = True
+x, y, z = 0, 1, 2
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(x), type(y), type(z))
+
+print(len(first_name))
+
+print(len(first_name) > len(last_name))
+
+num_one, num_two = 5, 4
+
+total = num_one + num_two
+diff = num_one - num_two
+product = num_one * num_two
+division = num_one / num_two
+remainder = num_one % num_two
+exp = num_one**num_two
+floor_division = num_one // num_two
+
+pi = 3.14
+area_of_circle = pi * 30**2
+circum_of_circlue = 2 * pi * 30
+print(pi * float(input("Radius: "))**2)
+
+first_name = input("First name: ")
+last_name = input("Last name: ")
+country = input("Country: ")
+age = int(input("Age: "))
